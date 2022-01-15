5066d1fa47cd37ba2c9232a857fa96b4b1121d28
Sibgat <sibgatulislam@gmail.com>
2022-01-15 22:54:59 +0600

    finished

---
+++ src/main/java/calculatorApp/CalculatorMain.java
@@ -0,0 +1,32 @@
+package calculatorApp;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class CalculatorMain extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(CalculatorMain.class.getResource("calculator-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.setFill(Color.TRANSPARENT);
+
+        stage.setTitle("Calculator");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        ((CalculatorController)fxmlLoader.getController()).init(stage);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
